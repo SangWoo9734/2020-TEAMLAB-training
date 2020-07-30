cb85e1bceb5a28d6c1988dcce3bbf7b9432b302b
sangwoo <psu8430@gmail.com>
2020-07-31 00:40:31 +0900

    Update At 0731

--- Quest4/market_action.js
+++ Quest4/market_action.js
@@ -1,34 +1,27 @@
 let colors = ["blue", "yellow", "pink"];
 let sizes = ["small", "medium", "large"];
 let types = ['p', 's', 't'];
-let sex = ['Man', 'Woman'];
+let gender = ['Man', 'Woman'];
 
 const products = [];
 
 for (let i of colors) {
    for (let j of sizes){
         for (let k of types){
-            products.push({color:i, size:j, type:k, sex:randomItem(sex) });
+            products.push({color:i, size:j, type:k, gender:randomItem(gender) });
         }
     }
 }
-const button1 = products.filter((product, index) => product.type === 't');
-const button2 = products.filter((product, index) => product.type === 's');
-const button3 = products.filter((product, index) => product.type === 'p');
-const button4 = products.filter((product, index) => product.color === 'blue');
-const button5 = products.filter((product, index) => product.color === 'yellow');
-const button6 = products.filter((product, index) => product.color === 'pink');
+function filter(id) {
+    return products.filter((product, index) => 
+    ((product.type === id) || (product.color === id)));
+}
 
-var selectedItem = document.getElementById("products");
-console.log(selectedItem);
-
-function showProducts(id) {
+function showProducts(ids) {
     var selectedItem = document.getElementById("products");
-    var addForm = document.createElement("div");
-        selectedItem.innerHTML = ""
-    for (let product of id){
-        selectedItem.innerHTML += "<div class = \"products-frame\"><img src=\"imgs/" + product.color + "_" + product.type + ".png\" alt=\"button1\" class = \"products-image\"><div class = \"products-discribe\">" +product.sex+", " + product.size + "-size </div></div>\n"
-        console.log(product);
+    selectedItem.innerHTML = ""
+    for (let product of ids){
+        selectedItem.innerHTML += "<div class = \"products-frame\"><img src=\"imgs/" + product.color + "_" + product.type + ".png\" alt=\"button1\" class = \"products-image\"><div class = \"products-discribe\">" +product.gender+", " + product.size + "-size </div></div>\n";
     }
     
 }
